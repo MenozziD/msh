d3f9a53ff8bf98a185b57974dd2b08f154226bb8
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-05-15 13:32:26 +0200

    Create deploy.sh

---
+++ Docker/esp_rele/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker container stop esprele
+docker container ls --all | grep esprele | awk '{print $1}' | xargs docker container rm
+docker build . --tag=esprele:v0.0.1
+docker run -d --name esprele -p 8081:8080 esprele:v0.0.1
