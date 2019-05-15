b39d916af9d2039d50f1308d8f03c7ba1dc23580
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-05-15 13:33:31 +0200

    Update deploy.sh

--- Docker/target_image/deploy.sh
+++ Docker/target_image/deploy.sh
@@ -3,6 +3,6 @@
 rm -r msh
 cp -r ../../msh .
 docker container stop raspberrypi
-docker container ls --all | awk '{print $1}' | xargs docker container rm
-docker build . --tag=msrheal:v0.0.1
-docker run -d --name raspberrypi -p 8080:65177 msrheal:v0.0.1
+docker container ls --all | grep raspberrypi | awk '{print $1}' | xargs docker container rm
+docker build . --tag=raspberrypi:v0.0.1
+docker run -d --name raspberrypi -p 8080:65177 raspberrypi:v0.0.1
