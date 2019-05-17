2951d54c084b53bd5e3beeeff693828fdf38bc53
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-05-17 17:03:54 +0200

    Update home.js

--- msh/webui/js/home.js
+++ msh/webui/js/home.js
@@ -14,7 +14,7 @@ function invia(){
 	} else {
 	    console.log("Tutto ok, faccio la chiamata");
 		$.ajax({
-		    url: "/api/netcmd",
+		    url: "/api/net_cmd",
 		    type: 'GET',
 		    data: {
 				d: device,
@@ -22,7 +22,11 @@ function invia(){
 			},
 		    success: function(response){
 				var json = $.parseJSON(JSON.stringify(response));
-				$('#result')[0].value = json["device_status"];
+				if (json["output"].search(":") > -1){
+                    $('#result')[0].value = json["output"].split(": ")[1];
+                } else {
+                    $('#result')[0].value = json["output"];
+                }
             },
             error: function(xhr){
             }
@@ -32,14 +36,49 @@ function invia(){
 
 function scan(){
     $.ajax({
-        url: "/api/netscan",
+        url: "/api/net_scan",
         type: 'GET',
         success: function(response){
             var json = $.parseJSON(JSON.stringify(response));
-            $('#esito')[0].value = json["output"];
+            if (json["output"].search(":") > -1){
+                $('#esito')[0].value = json["output"].split(": ")[1];
+            } else {
+                $('#esito')[0].value = json["output"];
+            }
             $('#found')[0].value = json["find_device"];
             $('#new')[0].value = json["new_device"];
             $('#update')[0].value = json["updated_device"];
+        },
+        error: function(xhr){
+        }
+    });
+}
+
+function list(){
+    $.ajax({
+        url: "/api/device_list",
+        type: 'GET',
+        success: function(response){
+            var json = $.parseJSON(JSON.stringify(response));
+            if (json["output"].search(":") > -1){
+                $('#errore').text(json["output"].split(": ")[1]);
+            } else {
+                $('#errore').text("");
+                var devices = json["devices"]
+                $("#table tbody").empty();
+                for(var i = 0; i < devices.length;i++) {
+                    device = devices[i];
+                    $('#table tbody').append('<tr>');
+                    $('#table tbody').append('<td>' + device['device_code'] + '</td>');
+                    $('#table tbody').append('<td>' + device['device_type'] + '</td>');
+                    $('#table tbody').append('<td>' + device['device_status'] + '</td>');
+                    $('#table tbody').append('<td>' + device['device_ip'] + '</td>');
+                    $('#table tbody').append('<td>' + device['device_mac'] + '</td>');
+                    $('#table tbody').append('<td>' + device['device_mac_info'] + '</td>');
+                    $('#table tbody').append('</tr>');
+                }
+                $('#table')[0].classList.remove('invisible');
+            }
         },
         error: function(xhr){
         }
