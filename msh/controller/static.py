6612bb253f6d6190d28890a88f41df9004d5d2d2
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-05-23 16:10:34 +0200

    Path cablati in static

--- msh/controller/static.py
+++ msh/controller/static.py
@@ -1,18 +1,17 @@
 from webapp3 import RequestHandler
 from logging import info, exception
-from module.xml_reader import XmlReader
 
 
 class Index(RequestHandler):
     def get(self):
         info("%s %s", self.request.method, self.request.url)
-        self.redirect(XmlReader.settings['path']['index'])
+        self.redirect('/static/page/index.html')
         info("RESPONSE CODE: %s to %s", self.response.status, self.response.headers['Location'])
 
 
 class Static(RequestHandler):
     def get(self, filename):
-        path_ui = XmlReader.settings['path']['ui']
+        path_ui = 'webui/'
         info("%s %s", self.request.method, self.request.url)
         f = open(path_ui + filename, 'r')
         self.response.write(f.read())
@@ -22,9 +21,13 @@ class Static(RequestHandler):
 
 
 def handle_error(request, response, excep):
-    path_error = XmlReader.settings['path']['error']
+    path_error = 'webui/page/error/'
     error_code = '500'
-    for code, value in XmlReader.settings['class_error'].items():
+    class_error = {
+        '404': '<class \'webob.exc.HTTPNotFound\'>',
+        '405': '<class \'webob.exc.HTTPMethodNotAllowed\'>'
+    }
+    for code, value in class_error.items():
         if str(value) == str(type(excep)):
             error_code = code
     if error_code != '500':
