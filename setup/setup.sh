349b50468a024161f9a125ce3f7bc88ecaedcdd9
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-05-25 13:01:51 +0200

    Add npm, ngrok e gactions in setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
 # UPDATE
-apt-get update -y
+sudo apt-get update -y
 # APT-UTILS
-apt-get install apt-utils -y
+sudo apt-get install apt-utils -y
 # PS, TOP, ecc..
-apt-get install procps -y
+sudo apt-get install procps -y
 # NETSTAT
-apt-get install net-tools -y
+sudo apt-get install net-tools -y
 # CURL
-apt-get install curl -y
+sudo apt-get install curl -y
 # NMAP
-apt-get install nmap -y
+sudo apt-get install nmap -y
 # SSH
-apt-get install ssh -y
+sudo apt-get install ssh -y
 # WAKEONLAN
-apt-get install wakeonlan -y
+sudo apt-get install wakeonlan -y
 # SAMBA per comando NET
-apt-get install samba-common-bin -y
+sudo apt-get install samba-common-bin -y
+# NPM AND NGROK
+curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
+sudo apt-get install npm -y
+sudo npm install ngrok -g
+# GACTIONS
+curl https://dl.google.com/gactions/updates/bin/linux/arm/gactions --output /usr/bin/gactions
+chmod +x /usr/bin/gactions
 # PYTHON
-apt-get install python3 -y
-apt-get install python3-pip -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
 pip3 install --trusted-host pypi.python.org webapp3
 pip3 install --trusted-host pypi.python.org paste
 pip3 install --trusted-host pypi.python.org pexpect
 pip3 install --trusted-host pypi.python.org netifaces
 # SQLITE
-apt-get install sqlite3 libsqlite3-dev -y
-# SUDO
-apt-get install sudo -y
+sudo apt-get install sqlite3 libsqlite3-dev -y
 
 cd ../msh
 mkdir db
@@ -36,9 +41,3 @@ cd db
 sqlite3 ./system.db
 sqlite3 ./system.db < ../script/create.sql
 python3 msh.py
-if curl -I -X GET http://127.0.0.1:80/static/page/index.html | grep 200;
-	then
-		echo "INSTALLAZIONE RIUSCITA!!"
-	else
-		echo "INSTALLAZIONE KO!!"
-fi
