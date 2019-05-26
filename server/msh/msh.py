ee3f35278eb55005e616ffb2441219c627367c50
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-05-27 01:45:47 +0200

    Action.json

--- server/msh/msh.py
+++ server/msh/msh.py
@@ -38,25 +38,27 @@ def main():
     cont = f.read()
     f.close()
     old_hostname = cont.split("\"url\": \"")[1].split("\"")[0].split("/api/home")[0]
+    old_hostname_auth_token = cont.split("\"authenticationUrl\": \"")[1].split("\"")[0].split("/oauth")[0]
     response = loads(request.urlopen("http://127.0.0.1:4040/api/tunnels").read().decode('utf-8'))
     new_hostname = ''
-    oauth_hostname = ''
+    new_hostname_auth_token = ''
     for tunnel in response['tunnels']:
         if tunnel['public_url'].find('https') == 0 and tunnel['config']['addr'].find(port) > 0:
             new_hostname = tunnel['public_url']
         if tunnel['public_url'].find('https') == 0 and tunnel['config']['addr'].find('3000') > 0:
-            oauth_hostname = tunnel['public_url']
+            new_hostname_auth_token = tunnel['public_url']
     if new_hostname != old_hostname:
         cont = cont.replace(old_hostname, new_hostname)
+        cont = cont.replace(old_hostname_auth_token, new_hostname_auth_token)
         f = open("action.json", "w")
         f.write(cont)
         f.close()
         system("gactions update --action_package action.json --project " + XmlReader.settings['project_id_google_actions'])
-        info("URL oauth: %s", oauth_hostname + "/oauth")
-        info("URL token: %s", oauth_hostname + "/token")
+        info("URL oauth: %s", new_hostname_auth_token + "/oauth")
+        info("URL token: %s", new_hostname_auth_token + "/token")
     info("Server in ascolto su http://%s:%s", ip_address, port)
     info("URL webapp: %s", new_hostname)
-    info("URL fake server %s", oauth_hostname)
+    info("URL fake server %s", new_hostname_auth_token)
     httpserver.serve(app, host=ip_address, port=port)
 
 
