86f5b70634906968cd32eb09606589ba80f3ba08
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-05-28 15:34:11 +0200

    Update init.py

--- server/msh/controller/__init__.py
+++ server/msh/controller/__init__.py
@@ -5,3 +5,4 @@ from controller.net_cmd import NetCmd
 from controller.net_device import NetDevice
 from controller.net_scan import NetScan
 from controller.static import Icon, Index, Static, handle_error
+from controller.user import User
