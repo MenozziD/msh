8493ef76454e20de38dd3a87d778752c010c4c57
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-05-29 16:42:46 +0200

    Update __init__.py

--- server/msh/module/__init__.py
+++ server/msh/module/__init__.py
@@ -1,3 +1,4 @@
 from module.xml_reader import XmlReader
 from module.dbmanager import DbManager
 from module.net import cmd_radio, cmd_ping, cmd_esp, cmd_netscan, cmd_pcwin_shutdown, cmd_radio_stato, cmd_wakeonlan, get_ip_and_subnet
+from module.datastore import add_user, update_user, delete_user
