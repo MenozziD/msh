690962b38105615098035803a691551be608cad2
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-05-31 10:48:58 +0200

    Update setupDocker.sh

--- script/setupDocker.sh
+++ script/setupDocker.sh
@@ -5,17 +5,17 @@ if [ "$#" -ne 5 ]; then
   exit 1
 fi
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get update -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 apt-cache madison docker-ce
-sudo apt-get install docker-ce=5:18.09.1~3-0~debian-stretch docker-ce-cli=5:18.09.1~3-0~debian-stretch containerd.io
-apt-get install qemu-user
+sudo apt-get install docker-ce=5:18.09.1~3-0~debian-stretch docker-ce-cli=5:18.09.1~3-0~debian-stretch containerd.io -y
+apt-get install qemu-user -y
 cd ../Docker/base_image
 sudo docker build . --tag=msh:v0.0.1
 cp ../../server ../raspberry_image
