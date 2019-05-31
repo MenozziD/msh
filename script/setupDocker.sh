0db46f38a395352fe0a65bfeab784bdbe94694bd
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-05-31 11:39:20 +0200

    Update setupDocker.sh

--- script/setupDocker.sh
+++ script/setupDocker.sh
@@ -5,6 +5,7 @@ if [ "$#" -ne 5 ]; then
   exit 1
 fi
 
+# INSTALLO DOCKER
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
 sudo apt-get update -y
 sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
@@ -15,10 +16,24 @@ sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 apt-cache madison docker-ce
 sudo apt-get install docker-ce=5:18.09.1~3-0~debian-stretch docker-ce-cli=5:18.09.1~3-0~debian-stretch containerd.io -y
+sudo service docker start
+# IMPOSTO DOCKER PER PARTIRE AL REBOOT
+sudo update-rc.d docker enable
+# INSTALLO QEMU
 apt-get install qemu-user -y
-cd ../Docker/base_image
-sudo docker build . --tag=msh:v0.0.1
-cp ../../server ../raspberry_image
-cd ../raspberry_image
-sudo docker build . --build-arg google_actions_project_id=$1 --build-arg user=$2 --build-arg password=$3 --build-arg dominio_oauth=$4 --build-arg dominio_webapp=$5 --tag=raspberrypi:v0.0.1
-sudo docker run -d --name raspberrypi -p 8080:65177 raspberrypi:v0.0.1
+# CREO IMMAGINE BASE ARM
+cd ../Docker/base_image_ARM
+sudo docker build . --tag=msh-arm:v0.0.1
+# CREO IMMAGINE BASE X64
+cd ../base_image_x64
+sudo docker build . --tag=msh-x64:v0.0.1
+# CREO ED ESEGUO IMMAGINE RASPBERRY ARM
+cp ../../server ../raspberry_image_ARM
+cd ../raspberry_image_ARM
+sudo docker build . --build-arg google_actions_project_id=$1 --build-arg user=$2 --build-arg password=$3 --build-arg dominio_oauth=$4 --build-arg dominio_webapp=$5 --tag=raspberry-arm:v0.0.1
+sudo docker run -d --name raspberry-arm raspberry-arm:v0.0.1
+# CREO ED ESEGUO IMMAGINE RASPBERRY X64
+cp ../../server ../raspberry_image_x64
+cd ../raspberry_image_x64
+sudo docker build . --build-arg google_actions_project_id=$1 --build-arg user=$2 --build-arg password=$3 --build-arg dominio_oauth=$4 --build-arg dominio_webapp=$5 --tag=raspberry-x64:v0.0.1
+sudo docker run -d --name raspberry-x64 raspberry-x64:v0.0.1
