0af05585eed32aff253a2c6a959fe5c7190b0847
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-06-07 12:35:29 +0200

    Delete install_arduino-cli-esp.sh

--- script/install_arduino-cli-esp.sh
+++
@@ -1,4 +0,0 @@
-sudo arduino-cli board listall
-sudo arduino-cli compile --fqbn esp8266:esp8266:generic test
-usb=`arduino-cli board list | grep tty | awk '{print $1}'`
-sudo arduino-cli upload -p $usb --fqbn esp8266:esp8266:generic test
