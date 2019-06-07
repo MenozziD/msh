5d3b3656774c6d2757748cd15884dbe38b235831
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-06-07 12:39:33 +0200

    Update __init__.py

--- server/msh/controller/__init__.py
+++ server/msh/controller/__init__.py
@@ -6,3 +6,4 @@ from controller.net_device import NetDevice
 from controller.net_scan import NetScan
 from controller.static import Icon, Index, Static, handle_error
 from controller.user import User
+from controller.upload_arduino import UploadArduino
