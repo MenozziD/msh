38a8c7b1ed9861c0d509c27be6143388d48273ee
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-06-08 00:46:25 +0200

    Fix deploy

--- script/deploy/deploy.bat
+++ script/deploy/deploy.bat
@@ -4,6 +4,6 @@ SET ZIPCMD="C:\\Program Files (x86)\\7zip\\7z.exe"
 SET RASPBERRY_SERVER="Raspberry Pi0"
 SET PATH_DIR="C:\\Users\\Simone.simone\\Documents\\Repository\\msh\\server\\msh\\*"
 %ZIPCMD% a msh.zip %PATH_DIR% 1> nul 2> nul
-%WINSCPCMD% /script=script_raspberry.txt /parameter %RASPBERRY_SERVER% 1> nul 2> nul
+%WINSCPCMD% /script=script_raspberry.txt /parameter %RASPBERRY_SERVER% %CD% 1> nul 2> nul
 del msh.zip
 exit
