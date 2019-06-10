efb1fe69a990ecc8b53a0a9911222b30836f09f6
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-06-10 18:28:13 +0200

    test

--- script/deploy/deploy.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET WINSCPCMD="C:\\Program Files (x86)\\WinSCP\\WinSCP.com"
-SET ZIPCMD="C:\\Program Files (x86)\\7zip\\7z.exe"
-SET RASPBERRY_SERVER="Rasp_LOCAL"
-SET PATH_DIR="C:\\Progetti\\msh\\server\\msh\\*"
-%ZIPCMD% a msh.zip %PATH_DIR% 1> nul 2> nul
-%WINSCPCMD% /script=script_raspberry.txt /parameter %RASPBERRY_SERVER% %CD% 1> nul 2> nul
-del msh.zip
-exit
