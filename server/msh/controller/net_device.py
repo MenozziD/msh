1fb678d859b0f8a5588b9e71bb493519033437bb
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-06-14 17:42:44 +0200

    Update net_device.py

--- server/msh/controller/net_device.py
+++ server/msh/controller/net_device.py
@@ -13,63 +13,43 @@ class NetDevice(BaseHandler):
         info("BODY %s", body)
         response = {}
         try:
-            if self.session.get('user') is not None:
-                data = loads(body)
+            data = loads(body)
+            DbManager()
+            response = NetDevice.check(self.session.get('user'), self.session.get('role'), data)
+            if response['output'] == 'OK':
                 type_op = data['tipo_operazione']
-                codice = data['codice']
-                tipo = data['tipo']
-                mac = data['mac']
-                user = data['user']
-                password = data['password']
-                DbManager()
-                if type_op == 'list':
-                    devices = DbManager.select_tb_net_device()
-                    if self.session.get('role') != 'ADMIN':
-                        for device in devices:
-                            device['net_usr'] = ''
-                            device['net_psw'] = ''
-                    response['user_role'] = self.session.get('role')
-                    response['devices'] = devices
-                    response['output'] = 'OK'
-                if type_op == 'type':
-                    response['types'] = DbManager.select_tb_net_device_type()
-                    response['output'] = 'OK'
-                if type_op == 'command':
-                    response['commands'] = DbManager.select_tb_net_command_from_type(tipo)
-                    response['output'] = 'OK'
-                if type_op == 'update':
-                    if self.session.get('role') == 'ADMIN':
-                        devices = DbManager.select_tb_net_device()
-                        to_update = DbManager.select_tb_net_device(mac)
-                        if len(to_update) == 1:
-                            to_update = to_update[0]
-                            if codice != to_update['net_code']:
-                                trovato = False
-                                for device in devices:
-                                    if device['net_code'] == codice:
-                                        trovato = True
-                                        break
-                                if not trovato:
-                                    DbManager.update_tb_net_device(mac, net_code=codice, net_type=tipo, net_user=user, net_psw=password)
-                                    response['output'] = 'OK'
-                                else:
-                                    response['output'] = 'Esiste già un dispositivo con questo codice'
-                            else:
-                                DbManager.update_tb_net_device(mac, net_code=codice, net_type=tipo, net_user=user, net_psw=password)
-                                response['output'] = 'OK'
-                        else:
-                            response['output'] = 'Non esiste nessun device con questo mac address'
-                    else:
-                        response['output'] = 'Solo gli ADMIN possono aggiornare i dispositivi'
-                if type_op == 'delete':
-                    if self.session.get('role') == 'ADMIN':
-                        DbManager.delete_tb_net_device(mac)
-                        response['output'] = 'OK'
-                    else:
-                        response['output'] = 'Solo gli ADMIN possono rimuovere i dispositivi'
-                DbManager.close_db()
-            else:
-                response['output'] = 'Devi effettuare la login per utilizzare questa API'
+                tipo = ''
+                mac = ''
+                codice = ''
+                user = ''
+                password = ''
+                if 'codice' in data:
+                    codice = data['codice']
+                if 'tipo' in data:
+                    tipo = data['tipo']
+                if 'mac' in data:
+                    mac = data['mac']
+                if 'user' in data:
+                    user = data['user']
+                if 'password' in data:
+                    password = data['password']
+                if response['output'] == 'OK':
+                    funzioni = {
+                        'list': NetDevice.device_list,
+                        'type': NetDevice.device_type,
+                        'command': NetDevice.device_command,
+                        'update': NetDevice.device_update,
+                        'delete': NetDevice.device_delete,
+                    }
+                    parametri = {
+                        'list': [self.session.get('role')],
+                        'type': [],
+                        'command': [tipo],
+                        'update': [mac, codice, tipo, user, password],
+                        'delete': [mac]
+                    }
+                    response = funzioni[type_op](*parametri[type_op])
+            DbManager.close_db()
         except Exception as e:
             exception("Exception")
             response['output'] = str(e)
@@ -80,3 +60,135 @@ class NetDevice(BaseHandler):
             self.response.write(dumps(response, indent=4, sort_keys=True))
             info("RESPONSE CODE: %s", self.response.status)
             info("RESPONSE PAYLOAD: %s", response)
+
+    @staticmethod
+    def check(user, role, data):
+        response = {}
+        if 'tipo_operazione' in data and data['tipo_operazione'] in ('list', 'type', 'command', 'update', 'delete'):
+            response = NetDevice.check_user(user, role, data['tipo_operazione'])
+            if response['output'] == 'OK':
+                if data['tipo_operazione'] == 'command':
+                    response = NetDevice.check_tipo(data, required=True)
+                if data['tipo_operazione'] == 'delete':
+                    response = NetDevice.check_mac(data)
+                if data['tipo_operazione'] == 'update':
+                    response = NetDevice.check_mac(data)
+                    if response['output'] == 'OK':
+                        response = NetDevice.check_tipo(data)
+                        if response['output'] == 'OK':
+                            response = NetDevice.check_code(data)
+        else:
+            if 'tipo_operazione' in data:
+                response['output'] = 'Il campo tipo_operazione deve assumere uno dei seguenti valori: list, type, command, update, delete'
+            else:
+                response['output'] = 'Il campo tipo_operazione è obbligatorio'
+        return response
+
+    @staticmethod
+    def check_tipo(data, required=False):
+        response = {}
+        type_list = [d['type_code'] for d in DbManager.select_tb_net_device_type()]
+        if 'tipo' in data and data['tipo'] in type_list:
+            response['output'] = 'OK'
+        else:
+            if 'tipo' in data:
+                response['output'] = "Il campo tipo deve assumere uno dei seguenti valori: " + ', '.join(type_list)
+            else:
+                if required:
+                    response['output'] = "Per l'operazione scelta è obbligatorio il campo tipo"
+                else:
+                    response['output'] = 'OK'
+        return response
+
+    @staticmethod
+    def check_mac(data):
+        response = {}
+        mac_list = [d['net_mac'] for d in DbManager.select_tb_net_device()]
+        if 'mac' in data and data['mac'] in mac_list:
+            response['output'] = 'OK'
+        else:
+            if 'mac' in data:
+                response['output'] = "Il campo mac deve assumere uno dei seguenti valori: " + ', '.join(mac_list)
+            else:
+                response['output'] = "Per l'operazione scelta è obbligatorio il campo mac"
+        return response
+
+    @staticmethod
+    def check_code(data):
+        response = {}
+        trovato = False
+        devices = DbManager.select_tb_net_device()
+        to_update = DbManager.select_tb_net_device(data['mac'])[0]
+        response['output'] = "OK"
+        if 'codice' in data:
+            if data['codice'] != to_update['net_code']:
+                for device in devices:
+                    if device['net_code'] == data['codice']:
+                        trovato = True
+                        break
+                if not trovato:
+                    response['output'] = 'OK'
+                else:
+                    response['output'] = 'Esiste già un dispositivo con questo codice'
+        return response
+
+    @staticmethod
+    def check_user(user, role, tipo_operazione):
+        response = {}
+        if user is not None:
+            if tipo_operazione in ('update', 'delete'):
+                if role != 'ADMIN':
+                    response['output'] = 'La funzione richiesta può essere eseguita solo da un ADMIN'
+                else:
+                    response['output'] = 'OK'
+            else:
+                response['output'] = 'OK'
+        else:
+            response['output'] = 'Devi effettuare la login per utilizzare questa API'
+        return response
+
+    @staticmethod
+    def device_list(role):
+        devices = DbManager.select_tb_net_device()
+        if role != 'ADMIN':
+            for device in devices:
+                device['net_usr'] = ''
+                device['net_psw'] = ''
+        response = {
+            'user_role': role,
+            'devices': devices,
+            'output': 'OK'
+        }
+        return response
+
+    @staticmethod
+    def device_type():
+        response = {
+            'types': DbManager.select_tb_net_device_type(),
+            'output': 'OK'
+        }
+        return response
+
+    @staticmethod
+    def device_command(tipo):
+        response = {
+            'types': DbManager.select_tb_net_command_from_type(tipo),
+            'output': 'OK'
+        }
+        return response
+
+    @staticmethod
+    def device_update(mac, codice, tipo, user, password):
+        DbManager.update_tb_net_device(mac, net_code=codice, net_type=tipo, net_user=user, net_psw=password)
+        response = {
+            'output': 'OK'
+        }
+        return response
+
+    @staticmethod
+    def device_delete(mac):
+        DbManager.delete_tb_net_device(mac)
+        response = {
+            'output': 'OK'
+        }
+        return response
