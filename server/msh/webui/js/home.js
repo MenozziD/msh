47a92f0da840118933b6931b3cd41c04f4dbef85
Menoz <mnz3553@gmail.com>
2019-06-16 04:19:16 +0200

    Portto stato device a front end

--- server/msh/webui/js/home.js
+++ server/msh/webui/js/home.js
@@ -152,6 +152,7 @@ function net(type_op){
                     if (type_op == 'cmd'){
                         $('#result')[0].value = json["output"];
                         $('#cmd_result')[0].value = json["res_decode"]["res_result"];
+                        $('#stato')[0].value = json["res_decode"]["res_state"];
                         net('list');
                     }
                 } else {
