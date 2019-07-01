094110052f3b6e3a23d8454d7928d0ec038950c0
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-07-01 13:13:47 +0200

    Update execute_test.sh

--- script/test/execute_test.sh
+++ script/test/execute_test.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 cd msh
+sudo adduser test
+echo test | passwd test --stdin
 sudo sqlite3 ./db/test.db < create.sql
 sudo python3 -m coverage erase
 sudo python3 -m coverage run -m pytest --junitxml=test-report.xml
 sudo mv .coverage ..
 sudo mv test-report.xml ..
 sudo rm db/test.db
+sudo userdel test
 cd ..
 exit 0
