8853eefa6b2f23ae0ac9d037f77f47b2f5b5ecd9
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-07-01 13:15:18 +0200

    Update test_06_utility.py

--- server/msh/test/test_06_utility.py
+++ server/msh/test/test_06_utility.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from test import read_xml_prod
-from module import execute_os_cmd
+from module import execute_os_cmd, execute_ssh_cmd
 
 
 class TestUtility(TestCase):
@@ -20,3 +20,18 @@ class TestUtility(TestCase):
         read_xml_prod()
         response = execute_os_cmd("pwd", sys=True)
         self.assertEqual(response, {})
+
+    def test_execute_ssh_cmd_ko_login(self):
+        read_xml_prod()
+        response = execute_ssh_cmd('127.0.0.1', 'test_user', 'test1234', 'pwd')
+        self.assertEqual(response['output'], 'Credenzilai non valide')
+
+    def test_execute_ssh_cmd_ko_other(self):
+        read_xml_prod()
+        response = execute_ssh_cmd('127.0.0.2', 'test_user', 'test1234', 'pwd')
+        self.assertNotEqual(response['output'], "OK")
+
+    def test_execute_ssh_cmd_ok(self):
+        read_xml_prod()
+        response = execute_ssh_cmd('127.0.0.1', 'test_user', 'test_password', 'pwd')
+        self.assertEqual(response['output'], "OK")
