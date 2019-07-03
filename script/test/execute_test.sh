57fe6a8e59a3ce6b325990e515588f60720516b4
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-07-03 09:29:15 +0200

    Update execute_test.sh

--- script/test/execute_test.sh
+++ script/test/execute_test.sh
@@ -5,7 +5,7 @@ sudo useradd test_user
 echo 'test_user:test_password' | sudo chpasswd
 sudo sqlite3 ./db/test.db < create.sql
 sudo python3 -m coverage erase
-sudo python3 -m coverage run -m pytest --junitxml=test-report.xml
+sudo python3 -m coverage run --rcfile=setup.cfg -m pytest --junitxml=test-report.xml
 sudo mv .coverage ..
 sudo mv test-report.xml ..
 sudo rm db/test.db
