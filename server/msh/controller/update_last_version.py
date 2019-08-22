0597757a3c1dbc3f0f3058b159cfdc0b631826b8
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-08-22 18:54:42 +0200

    Update update_last_version.py

--- server/msh/controller/update_last_version.py
+++ server/msh/controller/update_last_version.py
@@ -1,18 +1,19 @@
 from controller import BaseHandler
 from logging import info
-from module import execute_os_cmd, set_api_response
+from module import execute_os_cmd, set_api_response, XmlReader, DbManager
 
 
 class UpdateLastVersion(BaseHandler):
     def get(self):
         response = {}
         info("%s %s", self.request.method, self.request.url)
+        DbManager()
         if self.session.get('user') is not None and self.session.get('role') == 'ADMIN':
             response['output'] = 'OK'
             cmd = "cd .. && sudo ./deploy.sh &"
             execute_os_cmd(cmd, sys=True)
         else:
-            response['output'] = 'Devi effettuare la login per utilizzare questa API'
+            response['output'] = DbManager.select_tb_string_from_lang_value(XmlReader.settings['lingua'], 25)
             if self.session.get('role') == 'USER':
-                response['output'] = 'La funzione richiesta può essere eseguita solo da un ADMIN'
+                response['output'] = DbManager.select_tb_string_from_lang_value(XmlReader.settings['lingua'], 26)
         set_api_response(response, self.response)
