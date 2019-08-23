0a1544b2795d0cf9a7070427ae1ed2f55cfaa9a3
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-08-23 15:56:18 +0200

    Update __init__.py

--- server/msh/test/__init__.py
+++ server/msh/test/__init__.py
@@ -1,1 +1,1 @@
-from test.utility import simulate_login_admin, simulate_login_user, read_xml, simulate_os_command, read_xml_prod, simulate_request_http
+from test.utility import simulate_login_admin, simulate_login_user, read_xml, simulate_os_command, read_xml_prod, simulate_request_http, prova
