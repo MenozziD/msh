f2bb1daf6ed1312e1405e32b6a7a937915d33723
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-08-23 15:57:39 +0200

    Update __init__.py

--- server/msh/controller/__init__.py
+++ server/msh/controller/__init__.py
@@ -1,8 +1,8 @@
 from controller.base import BaseHandler
-from controller.home import Home
 from controller.login import Login, Logout
 from controller.net import Net
 from controller.static import Icon, Index, Static, handle_error
 from controller.user import User
 from controller.upload_arduino import UploadArduino
 from controller.update_last_version import UpdateLastVersion
+from controller.home import Home
