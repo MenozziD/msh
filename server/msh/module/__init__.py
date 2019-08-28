1e755c58abea9bbf249be05c2bb8578be36f4b42
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-08-28 13:05:30 +0200

    Update __init__.py

--- server/msh/module/__init__.py
+++ server/msh/module/__init__.py
@@ -1,5 +1,5 @@
 from module.xml_reader import XmlReader
 from module.dbmanager import DbManager
 from module.utility import execute_os_cmd, set_api_response, validate_format, execute_ssh_cmd, execute_request_http, check_server_connection, evaluate, get_string, traverse, get_gateway
-from module.net import cmd_radio, cmd_ping, cmd_esp, cmd_netscan, cmd_pcwin_shutdown, cmd_radio_stato, cmd_wakeonlan, compile_arduino, upload_arduino
+from module.net import cmd_radio, cmd_ping, cmd_esp, cmd_netscan, cmd_radio_stato, compile_arduino, upload_arduino, cmd_pcwin
 from module.datastore import add_user, update_user, delete_user, verify_token
