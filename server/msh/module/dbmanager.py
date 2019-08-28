e2510f3403ef767eca903ae970fcff77140203a2
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2019-08-28 13:05:11 +0200

    Update dbmanager.py

--- server/msh/module/dbmanager.py
+++ server/msh/module/dbmanager.py
@@ -112,7 +112,7 @@ class DbManager:
     def select_tb_net_command_from_type(net_type):
         query = 'SELECT * ' \
                 'FROM TB_NET_DIZ_CMD ' \
-                'WHERE CMD_NET_TYPE = \'%s\';' % net_type
+                'WHERE CMD_NET_TYPE = \'%s\' OR CMD_NET_TYPE = \'NET\';' % net_type
         net_diz_cmd = DbManager.select(query)
         diz_cmd = []
         for net_cmd in net_diz_cmd:
@@ -137,29 +137,23 @@ class DbManager:
         return list_stringhe[0]
 
     @staticmethod
-    def select_tb_net_device_tb_net_diz_cmd_from_code_and_cmd(net_code, cmd_str):
-        query = 'SELECT * ' \
-                'FROM TB_NET_DEVICE AS DEV INNER JOIN TB_NET_DIZ_CMD AS DIZ ON DEV.NET_TYPE = DIZ.CMD_NET_TYPE ' \
-                'WHERE DEV.NET_CODE = \'%s\' ' \
-                'AND DIZ.CMD_STR = \'%s\';' % (net_code, cmd_str)
-        net_devices_net_diz_cmd = DbManager.select(query)
-        devices_diz_cmd = []
-        for net_device in net_devices_net_diz_cmd:
-            tb_net_device = {
-                'net_code': str(net_device[0]),
-                'net_desc': str(net_device[1]),
-                'net_type': str(net_device[2]),
-                'net_last_update': str(net_device[3]),
-                'net_ip': str(net_device[4]),
-                'net_mac': str(net_device[5]),
-                'net_usr': str(net_device[6]),
-                'net_psw': str(net_device[7]),
-                'net_mac_info': str(net_device[8]),
-                'cmd_str': str(net_device[9]),
-                'cmd_net_type': str(net_device[10]),
-                'cmd_result': str(net_device[11])}
-            devices_diz_cmd.append(tb_net_device)
-        return devices_diz_cmd[0]
+    def select_device_and_function_code_from_code_and_cmd(net_code, cmd_str):
+        query = 'SELECT NET_TYPE, NET_IP, NET_MAC, NET_USER, NET_PSW ' \
+                'FROM TB_NET_DEVICE' \
+                'WHERE NET_CODE = \'%s\';' % net_code
+        device = DbManager.select(query)[0]
+        query = 'SELECT TYP.FUNCTION_CODE ' \
+                'FROM TB_NET_DEVICE_TYPE AS TYP INNER JOIN TB_NET_DIZ_CMD AS DIZ ON TYP.TYPE_CODE = DIZ.CMD_NET_TYPE ' \
+                'WHERE (TYP.TYPE_CODE IN (\'%s\', \'NET\') ' \
+                'AND DIZ.CMD_STR = \'%s\';' % (device[0], cmd_str)
+        net_device = {
+            'function_code': str(DbManager.select(query)[0][0]),
+            'net_ip': str(device[1]),
+            'net_mac': str(device[2]),
+            'net_usr': str(device[3]),
+            'net_psw': str(device[4])
+        }
+        return net_device
 
     @staticmethod
     def update_tb_net_device(net_mac, net_code=None, net_type=None, net_ip=None, net_user=None, net_psw=None, net_mac_info=None):
