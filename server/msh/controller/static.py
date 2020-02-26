28d1f0e51c6e0b662e24796dbaeb31692c11af95
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2020-02-26 16:41:20 +0100

    Update static.py

--- server/msh/controller/static.py
+++ server/msh/controller/static.py
@@ -7,22 +7,22 @@ from module import XmlReader
 class Index(BaseHandler):
     def get(self):
         info("%s %s", self.request.method, self.request.url)
-        self.redirect(XmlReader.settings['domain'] + '/static/page/index.html')
+        self.redirect(XmlReader.settings['protocol'] + self.request.url.split("//")[1].split("/")[0] + '/static/page/index.html')
         info("RESPONSE CODE: %s to %s", self.response.status, self.response.headers['Location'])
 
 
 class Icon(BaseHandler):
     def get(self):
         info("%s %s", self.request.method, self.request.url)
-        self.redirect(XmlReader.settings['domain'] + '/static/image/hub.png')
+        self.redirect(XmlReader.settings['protocol'] + self.request.url.split("//")[1].split("/")[0] + '/static/image/hub.png')
         info("RESPONSE CODE: %s to %s", self.response.status, self.response.headers['Location'])
 
 
 class Static(BaseHandler):
     def get(self, filename):
+        info("%s %s", self.request.method, self.request.url)
         if self.session.get('user') is not None or filename.find("login") > 0 or filename.find("hub.png") > 0:
             path_ui = 'webui/'
-            info("%s %s", self.request.method, self.request.url)
             f = open(path_ui + filename, 'rb')
             self.response.body = f.read()
             f.close()
@@ -30,7 +30,7 @@ class Static(BaseHandler):
             info("RESPONSE CODE: %s", self.response.status)
             info("RESPONSE PAYLOAD: %s%s", path_ui, filename)
         else:
-            self.redirect(XmlReader.settings['domain'] + '/static/page/login.html')
+            self.redirect(XmlReader.settings['protocol'] + self.request.url.split("//")[1].split("/")[0] + '/static/page/login.html')
             info("RESPONSE CODE: %s to %s", self.response.status, self.response.headers['Location'])
 
 
