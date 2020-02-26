c8d80d07dec1d19598e96b2197960a6b2cd35b90
Simone Sganzerla <simone.sganzerla.93@hotmail.it>
2020-02-26 16:42:34 +0100

    Update msh.py

--- server/msh/msh.py
+++ server/msh/msh.py
@@ -41,20 +41,34 @@ class Msh:
             format=XmlReader.settings['log']['format'],
             level=XmlReader.settings['log']['level'])
         porta = '65177'
-        Msh.start_service('ps -aux | grep node | grep server.js', 'oauth')
+        Msh.start_service('oauth')
         if Msh.check_server_connection("http://www.google.com", 15, 5):
-            Msh.start_dns_service("http://serveo.net", "serveo", 'ps -aux | grep serveo | grep 65177', '.serveo.net')
-            Msh.start_dns_service("http://pagekite.net", "pagekite", 'ps -aux | grep pagekite.py | grep python', '.pagekite.me')
+            for servizio in XmlReader.settings['dns']:
+                if servizio['abil'] == 's':
+                    Msh.start_dns_service(servizio)
         if len(Msh.service_avaiable) == 0:
             info("Avvio solo in locale")
+            XmlReader.settings['protocol'] = 'http://'
             ip_address = ifaddresses(get_gateway())[AF_INET][0]['addr']
         else:
+            XmlReader.settings['protocol'] = 'https://'
             ip_address = 'localhost'
             info("URL webapp: %s",  ', '.join(Msh.webapp_urls))
             info("URL oauth %s", ', '.join(Msh.oauth_urls))
         info("Server in ascolto su http://%s:%s", ip_address, porta)
         if XmlReader.settings["ambiente"] == 'PROD':
             httpserver.serve(Msh.app, host=ip_address, port=porta)  # pragma: no cover
+
+    @staticmethod
+    def start_dns_service(servizio):
+        if Msh.check_server_connection(servizio['test_url'], 2, 2):
+            Msh.start_service(servizio['name'])
+            oauth_url = "https://" + servizio['subdomain_oauth'] + servizio['domain']
+            webapp_url = "https://" + servizio['subdomain_webapp'] + servizio['domain']
+            if not Msh.final_check(servizio['name'], oauth_url):
+                Msh.service_avaiable.append(servizio['name'])
+                Msh.oauth_urls.append(oauth_url)
+                Msh.webapp_urls.append(webapp_url)
 
     @staticmethod
     def check_server_connection(url, tentativi, riposo):
@@ -73,35 +87,21 @@ class Msh:
         return server_on
 
     @staticmethod
-    def start_service(cmd_live, name):
-        response = execute_os_cmd(cmd_live, check_out=True)
-        if len(response['cmd_out'].split("\n")) <= 2:
+    def start_service(name):
+        response = execute_os_cmd("sudo service " + name + " check", check_out=True)
+        if len(response['cmd_out']) == "Spento":
             execute_os_cmd("sudo service " + name + " start")
         else:
             info("%s is already running", name)
 
     @staticmethod
-    def final_check(name, dominio):
+    def final_check(name, oauth_url):
         local = False
-        oauth_url = "https://" + XmlReader.settings['subdomain_oauth_' + name] + dominio
-        webapp_url = "https://" + XmlReader.settings['subdomain_webapp_' + name] + dominio
-        if not Msh.check_server_connection("https://" + XmlReader.settings['subdomain_oauth_' + name] + dominio + "/login", 3, 5):
+        if not Msh.check_server_connection(oauth_url + "/login", 3, 5):
             execute_os_cmd("sudo service " + name + " restart")
-            if not Msh.check_server_connection("https://" + XmlReader.settings['subdomain_oauth_' + name] + dominio + "/login", 3, 5):
+            if not Msh.check_server_connection(oauth_url + "/login", 3, 5):
                 local = True
-                oauth_url = ''
-                webapp_url = ''
-        return local, oauth_url, webapp_url
-
-    @staticmethod
-    def start_dns_service(test_url, name, cmd_live, dominio):
-        if Msh.check_server_connection(test_url, 2, 2):
-            Msh.start_service(cmd_live, name)
-            local, oauth_url, webapp_url = Msh.final_check(name, dominio)
-            if not local:
-                Msh.service_avaiable.append(name)
-                Msh.oauth_urls.append(oauth_url)
-                Msh.webapp_urls.append(webapp_url)
+        return local
 
 
 if __name__ == '__main__':
